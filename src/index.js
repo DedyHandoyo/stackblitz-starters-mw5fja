41a16ae1816ca4a55829e32b9598241183768a03
DedyHandoyo <108994387+DedyHandoyo@users.noreply.github.com>
2023-05-28 13:14:34 +0700

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 export function sum(a, b) {
   return a + b;
 }
+
+export class Example {
+  constructor(string, bool) {
+    this.string = string;
+    this.bool = bool;
+  }
+}
--- unit-test/try.test.js
+++ unit-test/try.test.js
@@ -1,5 +1,11 @@
-import {sum} from "../src/index.js";
+import {sum, Example} from "../src/index.js";
 
 test("test sum function", () => {
   expect(sum(10, 20)).toBe(30);
+});
+
+test("Test Example class", () => {
+  const example = new example("Dedy", true);
+
+  expect(example.name).toBe("Dedy");
 });
